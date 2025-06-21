b569fd7c5c39869d3416b85ec52cd92cb9c30cd4
Vivacity <134852538+Vivasity@users.noreply.github.com>
2025-06-21 12:55:37 +0800

    Update test0.py

--- test0.py
+++ test0.py
@@ -3,7 +3,7 @@ class Try(Scene):
     def construct(self):
         label0 = Text("Hello Manim!", font_size = 60, color = WHITE)
         s = Square(side_length = 2.5, color = BLUE, fill_opacity = 0.5)
-        c = Circle(radius = 2.5, color = RED, fill_opacity = 0.7)
+        c = Circle(radius = 2.5, color = RED, fill_opacity = 0.3)
         label1 = Text("This is a square.", font_size = 36, color = WHITE).next_to(s, DOWN)
         label2 = Text("This is a circle.", font_size = 36, color = WHITE).next_to(c, DOWN)
         self.play(Write(label0))
